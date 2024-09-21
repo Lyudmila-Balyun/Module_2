f676c75bbaad8aebc9285cacb4254e84f3013894
Людмила <severianka1@yandex.ru>
2024-09-21 20:03:09 +0300

    lesson7

---
+++ module_2_2.py
@@ -0,0 +1,8 @@
+first = input('Введите первое число: ') #или можно просто задать числа, напр first = 1, second = 2 и т.д.
+second = input('Введите второе число: ')
+third = input('Ввдите третье число: ')
+if (first) == (second) == (third):
+    print (3)
+elif (first) == (second) or (first) == (third) or (second) == (third):
+    print (2)
+else: print(0)
